e34dd82f3bd16d5adbe951c32f6fceb4e96a8fd0
bayliewarrick <57427446+bayliewarrick@users.noreply.github.com>
2019-11-07 14:54:18 -0600

    Added looping to the calculator program.

--- calculator.py
+++ calculator.py
@@ -1,4 +1,7 @@
 #calculates the result of two numbers and an operand.
+input1 = 0
+input3 = 0
+
 def takeinputs():
     input1 = int(input("Enter the first number for your calculation: "))
     input2 = input("Enter the operation desired (+, -, *, /):")
@@ -6,21 +9,16 @@ def takeinputs():
     return input1, input2, input3
 
 def add(input1, input3):
-    total = input1 + input3
-    return total    
-
+    return (input1 + input3)
+        
 def sub(input1, input3):
-    total = input1 - input3
-    return total
+    return (input1 - input3)
 
 def mul(input1, input3):
-    total = input1 * input3
-    return total  
+    return (input1 * input3) 
 
 def div(input1, input3):
-    total = input1 / input3
-    return total    
-
+    return (input1 / input3)    
 
 def calc():
     (input1,input2,input3) = takeinputs()
@@ -33,10 +31,19 @@ def calc():
         result = mul(input1, input3)
     elif input2 == "/":
         result = div(input1, input3)
-    else: 
-        input2 = input("Enter the operation desired (+, -, *, /):")
-        input3 = int(input("Enter the second number for your calculation: "))
+    else:
+        print("Invalid Operand! Please try again.")
+        calc()
     print(f"{input1} {input2} {input3} equals {result}")
-
+    again = input('Press "A" to calculate another equation. press any other key to exit program.').upper()
+    if(again == 'A'):
+        calc()
+    else:
+        print("Exiting Program!")
+        exit(1000)
+    
+    
 
 calc()
+
+
