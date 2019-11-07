8bb78004102aea4cd977d80d5eea0e6c42924d5a
bayliewarrick <57427446+bayliewarrick@users.noreply.github.com>
2019-11-07 12:53:39 -0600

    Finished assignments.

--- calculator.py
+++ calculator.py
@@ -5,16 +5,34 @@ def takeinputs():
     input3 = int(input("Enter the second number for your calculation: "))
     return input1, input2, input3
 
+def add(input1, input3):
+    total = input1 + input3
+    return total    
+
+def sub(input1, input3):
+    total = input1 - input3
+    return total
+
+def mul(input1, input3):
+    total = input1 * input3
+    return total  
+
+def div(input1, input3):
+    total = input1 / input3
+    return total    
+
+
 def calc():
     (input1,input2,input3) = takeinputs()
+
     if input2 == "+":
-        result = input1 + input3
+        result = add(input1, input3)
     elif input2 == "-":
-        result = input1 - input3
+        result = sub(input1, input3)
     elif input2 == "*":
-        result = input1 * input3
+        result = mul(input1, input3)
     elif input2 == "/":
-        result = input1 / input3
+        result = div(input1, input3)
     else: 
         input2 = input("Enter the operation desired (+, -, *, /):")
         input3 = int(input("Enter the second number for your calculation: "))
@@ -22,6 +40,3 @@ def calc():
 
 
 calc()
-
-
-
